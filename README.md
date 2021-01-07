bbd74935bb6a1c7830676b8ba8f8907bc3441db4
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-01-07 12:10:50 +0100

    Add function to check token & domainkey
    And add new styles

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-timev2chrome
+#Google Chrome Extension
+To give you the best experience with our app, we have programmed a Google Chrome extension. This accesses your app installation via the **domainkey** and the **token**.
+
+Currently only times can be added via the applications. Other functions are currently not planned.
+
+##manifest.json
+Here, as with any extension, the permissions and metadata are stored.
+
+##popup.html
+In the popup HTML only a form is stored and the possibility to make the settings.
+
+##main.js
+In the JavaScript file the brain of this extension is hidden. Everything is controlled here.
+
+The keys are read or stored here via the Google Chrome Storage. The customers and projects are loaded directly via two functions. And via the token our backend recognizes which user it is.
+
+The complete communication with our backend runs over this file.
+
+##style.sass / main.min.css
+Actually only serves to beautify the HTML file.
--- js/main.js
+++ js/main.js
@@ -7,87 +7,132 @@ window.addEventListener("load", () => {
         document.querySelector("#token").value = data.token
         document.querySelector("#domainkey").value = data.domainkey
 
-        // Load clients
-        fetch("https://" + document.querySelector("#domainkey").value + ".timev2.de/api/v1/clients", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "authorization": document.querySelector("#token").value
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data.clients.length; i++) {
-                let p = document.createElement("option")
-                p.innerText =data.clients[i].name
-                p.value = data.clients[i].id
-                document.querySelector("#clients").appendChild(p)
-            }
-        })
-    
-        // Load projects
-        fetch("https://" + document.querySelector("#domainkey").value + ".timev2.de/api/v1/projects", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "authorization": document.querySelector("#token").value
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data.projects.length; i++) {
-                let p = document.createElement("option")
-                p.innerText =data.projects[i].name
-                p.value = data.projects[i].id
-                document.querySelector("#projects").appendChild(p)
-            }
-        })
+        // Check chrome storage
+        if (data.token === undefined || data.domainkey === undefined || data.token.length <= 0 || data.domainkey.length <= 0) {
+
+            // Delete settings
+            document.querySelector("#token").value = ""
+            document.querySelector("#domainkey").value = ""
+
+            // Change css classes
+            document.querySelector("#form").classList.add("d-none")
+            document.querySelector("#settings").classList.toggle("d-none")
+        } else {
+
+            // Load clients
+            fetch("https://" + document.querySelector("#domainkey").value + ".timev2.de/api/v1/clients", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": document.querySelector("#token").value
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    for (let i = 0; i < data.clients.length; i++) {
+                        let p = document.createElement("option")
+                        p.innerText =data.clients[i].name
+                        p.value = data.clients[i].id
+                        document.querySelector("#clients").appendChild(p)
+                    }
+                })
+
+            // Load projects
+            fetch("https://" + document.querySelector("#domainkey").value + ".timev2.de/api/v1/projects", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": document.querySelector("#token").value
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    for (let i = 0; i < data.projects.length; i++) {
+                        let p = document.createElement("option")
+                        p.innerText =data.projects[i].name
+                        p.value = data.projects[i].id
+                        document.querySelector("#projects").appendChild(p)
+                    }
+                })
+
+        }
 
     })
   
   })
   
-  // Send data to server
-  document.querySelector("#send").addEventListener("click", () => {
-    let xhr = new XMLHttpRequest()
-    xhr.open("POST", "https://" + document.querySelector("#domainkey").value + ".timev2.de/api/v1/times", true)
-    xhr.setRequestHeader("Content-Type", "application/json")
-    xhr.setRequestHeader("authorization", document.querySelector("#token").value)
-    xhr.send(JSON.stringify({
-        "time": document.querySelector("#time").value,
-        "client": document.querySelector("#clients").value,
-        "project": document.querySelector("#projects").value,
-        "description": document.querySelector("#description").value
-    }))
-    xhr.onload = () => {
-        if (xhr.readyState === xhr.DONE) {
-            if (xhr.status === 200) {
-                location.reload()
-            } else {
-                document.querySelector("#error").classList.remove("d-none")
+    // Send data to server
+    document.querySelector("#send").addEventListener("click", () => {
+        if (document.querySelector("#time").value === "00:00" || document.querySelector("#clients").value.length <= 0 || document.querySelector("#projects").value.length <= 0 || document.querySelector("#description").value.length <= 0) {
+            document.querySelector("#errorfill").classList.remove("d-none")
+        } else {
+            let xhr = new XMLHttpRequest()
+            xhr.open("POST", "https://" + document.querySelector("#domainkey").value + ".timev2.de/api/v1/times", true)
+            xhr.setRequestHeader("Content-Type", "application/json")
+            xhr.setRequestHeader("authorization", document.querySelector("#token").value)
+            xhr.send(JSON.stringify({
+                "time": document.querySelector("#time").value,
+                "client": document.querySelector("#clients").value,
+                "project": document.querySelector("#projects").value,
+                "description": document.querySelector("#description").value
+            }))
+            xhr.onload = () => {
+                if (xhr.readyState === xhr.DONE) {
+                    if (xhr.status === 200) {
+                        location.reload()
+                    } else {
+                        document.querySelector("#errorsend").classList.remove("d-none")
+                    }
+                }
             }
         }
-    }
-  })
+    })
+
+    // Add animation to edit button
+    document.querySelector("#edit").addEventListener("mouseover", () => {
+
+        // Change css classes
+        document.querySelector("#edit").classList.add("animate__animated", "animate__heartBeat")
+
+        // Change back
+        setTimeout(() => {
+
+            // Change css classes
+            document.querySelector("#edit").classList.remove("animate__animated", "animate__heartBeat")
+
+        }, 1000)
+
+    })
   
-  // Open settings
-  document.querySelector("#edit").addEventListener("click", () => {
-      document.querySelector("#form").classList.add("d-none")
-      document.querySelector("#settings").classList.toggle("d-none")
-  })
+    // Open settings
+    document.querySelector("#edit").addEventListener("click", () => {
+
+        // Change css classes
+        document.querySelector("#form").classList.add("d-none")
+        document.querySelector("#settings").classList.toggle("d-none")
+
+    })
   
-  // Close settings
-  document.querySelector("#cancel").addEventListener("click", () => {
-      document.querySelector("#form").classList.toggle("d-none")
-      document.querySelector("#settings").classList.toggle("d-none")
-  })
-  
-  // Save data to chrome storage
-  document.querySelector("#save").addEventListener("click", () => {
-      chrome.storage.sync.set({
-          "domainkey": document.querySelector("#domainkey").value,
-          "token": document.querySelector("#token").value
-      })
-      document.querySelector("#form").classList.toggle("d-none")
-      document.querySelector("#settings").classList.toggle("d-none")
-  })
+    // Close settings
+    document.querySelector("#cancel").addEventListener("click", () => {
+
+        // Change css classes
+        document.querySelector("#form").classList.toggle("d-none")
+        document.querySelector("#form").classList.add("animate__animated", "animate__fadeIn")
+        document.querySelector("#settings").classList.toggle("d-none")
+
+    })
+
+    // Save data to chrome storage
+    document.querySelector("#save").addEventListener("click", () => {
+
+        // Set chrome storage
+        chrome.storage.sync.set({
+            "domainkey": document.querySelector("#domainkey").value,
+            "token": document.querySelector("#token").value
+        })
+
+        // reload page
+        location.reload()
+
+    })
